4a1d65c3f61d0e703e1d178fb4c4c5e7a0322fc5
zh-hongda <13253560783@163.com>
2020-08-26 17:27:35 +0800

    完成springboot与solr集群的连接。 从单机版，过渡到集群版。

---
+++ src/main/java/com/redgo/controller/SearchConfig.java
@@ -0,0 +1,18 @@
+package com.redgo.controller;
+
+import org.apache.solr.client.solrj.impl.CloudSolrClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SearchConfig {
+
+    @Value("${spring.data.solr.zk-host}")
+    private String zkHost;
+
+    @Bean
+    public CloudSolrClient solrClient() {
+        return new CloudSolrClient(zkHost);
+    }
+}
